fee0428b3ba8d2991d9e21936c5f28e59e7f1317
auliaramadhan <aul.dark@gmail.com>
2019-03-11 22:32:43 +0700

    kenapa bisa hilang

---
+++ netninja_graphql/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const schema = require("./schema");
+const mongoose = require('mongoose')
+
+
+const app = express();
+
+mongoose.connect('mongodb+srv://aul:<123>@learngrahql-hirkr.mongodb.net/test?retryWrites=true')
+mongoose.connection.once('open', () => {
+  
+})
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
+
+app.listen(4000, () => {
+  console.log("now listening port 400");
+});
+
